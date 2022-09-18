fb28529d7a260b0f0a899f71a1153142f11ce917
Muh. Fahrezza Novaldi <72784469+rezam5n@users.noreply.github.com>
2022-09-18 17:04:12 +0700

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -3,15 +3,15 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V IDM /T REG_SZ /D "C:\Program Files (x86)\Internet Download Manager\IDMan.exe" /F > out.txt 2>&1
-net user administrator Maldini123 /add >nul
+net user administrator 12345abc /add >nul
 net localgroup administrators administrator /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: Administrator
-echo Pass: Maldini123
-curl -O https://raw.githubusercontent.com/94maldini/Windows2019RDP-AP/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/94maldini/Windows2019RDP-AP/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+echo Pass: 12345abc
+curl -O https://raw.githubusercontent.com/rezam5n/rdp-trial/main/Files/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/rezam5n/rdp-trial/main/Files/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\idm6.40.11.exe" https://raw.githubusercontent.com/94maldini/file/main/idm6.40.11.exe > out.txt 2>&1
 FOR %%i IN ("C:\Users\Public\Desktop\idm*.exe") DO Set FileName="%%i" /S /EN
 %FileName% /SILENT
